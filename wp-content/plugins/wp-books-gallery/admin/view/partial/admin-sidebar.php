a69a16785ff1b8a7ba1c973c1279edcd8775eeb6
David Araújo <david2araujo5@gmail.com>
2022-09-09 23:17:51 +0100

    Update

---
+++ wp-content/plugins/wp-books-gallery/admin/view/partial/admin-sidebar.php
@@ -0,0 +1,45 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+?>
+<div class="wbg-admin-sidebar" style="width: 20%; float: left;">
+    <div class="postbox">
+        <h3 class="hndle"><span>Support / Bug / Customization</span></h3>
+        <div class="inside centered">
+            <p>Please feel free to let us know if you have any bugs to report. Your report / suggestion can make the plugin awesome!</p>
+            <p style="margin-bottom: 1px! important;"><a href="https://books-gallery.com/contact/" target="_blank" class="button button-primary">Get Support</a></p>
+        </div>
+    </div>
+    <div class="postbox">
+        <h3 class="hndle"><span>More About Books Gallery / Live Chat</span></h3>
+        <div class="inside centered">
+            <p style="margin-bottom: 1px! important;"><a href="https://books-gallery.com/" target="_blank" class="button button-primary">Pro Features</a></p>
+        </div>
+    </div>
+    <div class="postbox">
+        <h3 class="hndle"><span>Buy us a coffee</span></h3>
+        <div class="inside centered">
+            <p>If you like the plugin, would you like to support the advancement of this plugin?</p>
+            <p style="margin-bottom: 1px! important;"><a href='https://www.paypal.me/mhmrajib' class="button button-primary" target="_blank">Donate</a></p>
+        </div>
+    </div>
+    <div class="postbox">
+        <h3 class="hndle"><span>Join us on facebook</span></h3>
+        <div class="inside centered">
+        <p style="margin-bottom: 1px! important;"><a href='https://www.facebook.com/wpbooksgalleryplugin' class="button button-info" target="_blank">Join Books Gallery<span class="dashicons dashicons-facebook" style="position: relative; top: 3px; margin-left: 3px; color: #0fb9da;"></span></a></p>
+        </div>
+    </div>
+    <div class="postbox">
+        <h3 class="hndle"><span>Follow us on twitter</span></h3>
+        <div class="inside centered">
+            <a href="https://twitter.com/hmplugin" target="_blank" class="button button-secondary">Follow @hmplugin<span class="dashicons dashicons-twitter" style="position: relative; top: 3px; margin-left: 3px; color: #0fb9da;"></span></a>
+        </div>
+    </div>
+    <div class="postbox">
+        <h3 class="hndle"><span>Subscribe us on YouTube</span></h3>
+        <div class="inside centered">
+            <a href="http://www.youtube.com/channel/UC9UVmN-KZ6iuZypToFp_YTQ?sub_confirmation=1" target="_blank" class="button button-secondary">Subscribe HM Plugin<span class="dashicons dashicons-youtube" style="position: relative; top: 3px; margin-left: 3px; color: #CC0000;"></span></a>
+        </div>
+    </div>
+</div>
